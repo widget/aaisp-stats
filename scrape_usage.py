16c1b03ede824572b76e18437f3463838c1c43c7
Widget <widget@stdin.co.uk>
2014-09-01 20:41:23 +0100

    Reflowed graphs in lists, print time of generation, new peak/off-peak ratio

--- scrape_usage.py
+++ scrape_usage.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
 
 import argparse
-import requests
 from bs4 import BeautifulSoup
-import re
-from collections import namedtuple
-import json
-import pygal
 import calendar
-from datetime import date
+from collections import namedtuple
+import datetime
+import json
+import pickle
+import pygal
+import re
+import requests
 import sys
+import time
 
 MonthlyUsage = namedtuple("MonthlyUsage", ["month", "peak_down", "peak_up", "other_down", "other_up", "uptime"])
 BillingBasic = namedtuple("BillingBasic", ["start", "end", "bf", "used", "allowance", "topup", "details"])
+Usage = namedtuple("Usage", ["start", "duration","peak_down", "peak_up", "other_down", "other_up"])
 
 
 def convertWithPrefix(text):
+	if text == "":
+		return 0.0
 	num = float(text[:-1])
 	if text.endswith('M'):
 		num = num / 1024
@@ -31,6 +36,63 @@ def parseMonthlyUsageRow(row):
 	ou = convertWithPrefix(cells[4].text)
 	up = float(cells[5].text[:-1]) # cut the percentage
 	return MonthlyUsage(month, pd, pu, od, ou, up)
+
+
+def parseDailyUsageRow(row):
+	cells = row.find_all('td')
+	try:
+		st = datetime.datetime.strptime(row.find('th').text, "%a %d %b %Y") 
+		tu = convertWithPrefix(cells[0].text)
+		td = convertWithPrefix(cells[1].text)
+		pu = convertWithPrefix(cells[2].text)
+		pd = convertWithPrefix(cells[3].text)
+		ou = tu - pu
+		od = td - pd
+		return Usage(st,#datetime.datetime.fromtimestamp(time.mktime(st)),
+			   datetime.timedelta(days=1),
+					pd, pu, od, ou)
+	except ValueError:
+		return None
+	
+
+def parseHourlyUsageRow(row, up=False):
+	cells = row.find_all('td')
+	try:
+		start_cell=row.find('th')
+		start_time = datetime.datetime.strptime(start_cell.text, "%a %d %b %Y")
+		#start_time = datetime.datetime.fromtimestamp(time.mktime(st))
+		peakday = 'W' in start_cell["class"]
+		ret = []
+		for c in cells:
+			amt = convertWithPrefix(c.text)
+			hr = int(c["class"][0][1:])
+			if peakday:
+				peak = 9 <= hr <= 17
+			else:
+				peak = False
+				
+			if peak:
+				if up:
+					u = Usage(start_time + datetime.timedelta(hours=hr), 
+								datetime.timedelta(hours=1),
+								0, amt, 0, 0)
+				else:
+					u = Usage(start_time + datetime.timedelta(hours=hr), 
+								datetime.timedelta(hours=1),
+								amt, 0, 0, 0)
+			else:
+				if up:
+					u = Usage(start_time + datetime.timedelta(hours=hr), 
+								datetime.timedelta(hours=1),
+								0, 0, 0, amt)
+				else:
+					u = Usage(start_time + datetime.timedelta(hours=hr), 
+								datetime.timedelta(hours=1),
+								0, 0, amt, 0)
+			ret.append(u)
+		return ret
+	except ValueError:
+		raise
 
 
 def parseBillingBasicRow(row):
@@ -53,7 +115,8 @@ CLUELESS="https://clueless.aa.net.uk/"
 def fetchData(creds):
 	session = requests.Session()
 	session.auth = tuple(creds)
-	params = { "LINE" : "",
+	params = { "LINE" : "1",
+			"LIMIT" : "31",
 			"LOGIN" : creds[0]
 			}
 	r = session.get(CLUELESS + 'usage.cgi', params=params)
@@ -66,17 +129,27 @@ def fetchData(creds):
 
 	monthly_usage = table_list[-4]
 	billing = table_list[-3]
+	daily_usage = table_list[-2]
+	hourly_usage = table_list[-1]
 
 	assert "Month" in monthly_usage.text
 	assert "Period" in billing.text
 	
 	match=re.compile(r"OE[01]")
 	rows = monthly_usage.find_all('tr', class_=match)
-	usages = [parseMonthlyUsageRow(c) for c in rows]
+	usages_mon = [parseMonthlyUsageRow(c) for c in rows]
 	rows = billing.find_all('tr', class_=match)
 	billbasic = [parseBillingBasicRow(c) for c in rows]
 	
-	return usages, billbasic
+	rows = daily_usage.find_all('tr', class_=match)
+	usages_day = [parseDailyUsageRow(c) for c in rows]
+	rows = hourly_usage.find_all('tr', class_=match)
+	assert not len(rows) % 2
+	half = int(len(rows)/2)
+	
+	usages_hour = [parseHourlyUsageRow(c) for c in rows[:half]] + [parseHourlyUsageRow(c, True) for c in rows[half:]]
+	
+	return {"month" : usages_mon, "day" : usages_day, "hour" : usages_hour}, billbasic
 
 
 if __name__ == "__main__":
@@ -96,24 +169,25 @@ if __name__ == "__main__":
 			sys.exit(1)
 		creds = json.load(open(args.creds, "r"))
 		u, b = fetchData(creds)
-		json.dump(u, open("usages.json","w"))
-		json.dump(b, open("billing.json","w"))
+		pickle.dump(u, open("usages.bin","wb"))
+		pickle.dump(b, open("billing.bin","wb"))
 		
 	if args.graph:
-		u = json.load(open("usages.json"))
-		b = json.load(open("billing.json"))
-
-		usages = [MonthlyUsage(*obj) for obj in u]
-		billing = [BillingBasic(*obj) for obj in b]
+		usages = pickle.load(open("usages.bin", "rb"))
+		billing = pickle.load(open("billing.bin", "rb"))
 	
 		this_mon = billing[0]
 		
 		cal = calendar.Calendar()
-		days_in_period = [x for x in cal.itermonthdays(date.today().year, date.today().month) if x > 0]
-		passed = [x for x in days_in_period if x < date.today().day]
+		today = datetime.date.today()
+		days_in_period = [x for x in cal.itermonthdays(today.year, today.month) if x > 0]
+		passed = [x for x in days_in_period if x < today.day]
 		to_come = [x for x in days_in_period if x not in passed]
 		bill_ratio = float(len(passed))/float(len(days_in_period))
-		forecast = this_mon.used / bill_ratio
+	
+		forecast = 0.0
+		if bill_ratio > 0.0:
+			forecast = this_mon.used / bill_ratio
 	
 		our_text = "At this rate we will take %.2f units into next month" % -(this_mon.bf+forecast-this_mon.allowance)
 		over = False
@@ -123,11 +197,16 @@ if __name__ == "__main__":
 		elif forecast > this_mon.allowance:
 			our_text = "We are forecast to use more data than we buy each month"
 		
-		from pygal.style import RedBlueStyle
-		ourstyle = RedBlueStyle
+		from pygal.style import LightenStyle, LightColorizedStyle
+		ourstyle = LightenStyle('#235670', step=5, base_style=LightColorizedStyle)
+		ourstyle.background = '#ffffff'
 		
-		usage_pie = pygal.Pie(style=ourstyle)
-		usage_pie.title = "Current monthly usage"
+		conf = pygal.Config()
+		conf.style = ourstyle
+		conf.legend_at_bottom = True
+		
+		usage_pie = pygal.Pie(conf)
+		usage_pie.title = "Current monthly usage (units)"
 		if over:
 			usage_pie.add("Over-usage", this_mon.used - this_mon.allowance)
 			usage_pie.add("Allowance", this_mon.allowance)
@@ -135,8 +214,12 @@ if __name__ == "__main__":
 			usage_pie.add("Usage", this_mon.used)
 			usage_pie.add("Remaining", this_mon.allowance - this_mon.bf)
 		
+		pop = pygal.Pie(conf)
+		pop.title = "Peak/off-peak (units)"
+		pop.add("Peak", usages["month"][0].peak_down / 2.5)
+		pop.add("Off-peak", usages["month"][0].other_down / 50)
 	
-		time_cht = pygal.HorizontalStackedBar(style=ourstyle, show_y_labels=False, height=120, width=800)
+		time_cht = pygal.HorizontalStackedBar(conf, show_y_labels=False, height=120, width=800)
 		time_cht.title = "Distance through billing period"
 		time_cht.x_labels_major_every = 7
 		time_cht.x_labels_minor_every = 1
@@ -144,18 +227,44 @@ if __name__ == "__main__":
 		time_cht.add("Remaining", len(to_come))
 		
 		billing.reverse()
-		bf_chrt = pygal.Line(style=ourstyle, interpolate="cubic", height=400, width=800)
-		bf_chrt.title = "Historic usage"
+		bf_chrt = pygal.Line(conf, interpolate="cubic", height=400, width=800)
+		bf_chrt.title = "Monthly usage (units)"
 		bf_chrt.y_title = "Units"
 		bf_chrt.x_labels = [b.start for b in billing]
 		bf_chrt.add("Accrued", [-b.bf for b in billing])
 		bf_chrt.add("Used", [b.used for b in billing])
 		bf_chrt.add("Allowance", [b.allowance for b in billing])
 		
-		doc = """<html><title>Our usage</title>
+		usages_mon = usages["month"]
+		usages_mon.reverse()
+		
+		usage_mon_chrt = pygal.StackedLine(conf, logarithmic=True,
+									 x_label_rotation=45, fill=True,
+									 interpolate="cubic")
+		usage_mon_chrt.title = "Monthly usage (GB)"
+		#usage_mon_chrt.y_title = "Data"
+		usage_mon_chrt.x_labels = [u.month for u in usages_mon]
+		usage_mon_chrt.add("Peak upload", [u.peak_up for u in usages_mon])
+		usage_mon_chrt.add("Peak download",[u.peak_down for u in usages_mon])
+		usage_mon_chrt.add("Non-peak upload", [u.other_up for u in usages_mon])
+		usage_mon_chrt.add("Non-peak download", [u.other_down for u in usages_mon])
+		
+		day = [d for d in usages["day"] if d is not None]
+		day.reverse()
+		usage_day_chrt = pygal.StackedLine(conf, x_label_rotation=45, interpolate="hermite",
+											height=400, width=800)
+		usage_day_chrt.x_labels = [d.start.strftime("%a %d %b") for d in day]
+		usage_day_chrt.title = "Daily usage (GB)"
+		usage_day_chrt.add("Peak", [d.peak_down for d in day])
+		usage_day_chrt.add("Non-peak", [d.other_down for d in day])
+		
+		doc = """<html xmlns="http://www.w3.org/1999/xhtml">
+		<head>
+		<meta charset="UTF-8"/>
+		<title>Our usage</title>
 		<style>
 		p.title {
-			font-size:200%;
+			font-size:300%;
 			text-align: center;
 			font-family: monospace;
 		}
@@ -165,13 +274,14 @@ if __name__ == "__main__":
 			margin-bottom=2em;
 			margin-left=10%;
 			margin-right=10%;
+			font-size: 150%;
 		}
 		table#summary {
 			width:50%; 
 			margin-left:25%; 
 			margin-right:25%;
 			border-spacing: 1em;
-			font-size: 110%;
+			font-size: 200%;
 		}
 		#summary th {
 			font-family: sans-serif;
@@ -184,8 +294,28 @@ if __name__ == "__main__":
 			margin=5em;
 			
 		}
+		ul
+		{
+			list-style-type: none;
+			padding:0; margin:0;
+		}
+		li.horiz
+		{
+			display:table-cell; 
+			padding: 1px;
+			width: 500px;
+		}
 		</style>
-		<body>
+		<script>
+		function resizeMe() {
+			var elements = document.querySelectorAll('li.horiz');
+			for(var i = 0; i < elements.length; i++) {
+				elements[i].style.width = (screen.width/2) - 20 + "px";
+			}
+		}
+		</script>
+		</head>
+		<body onload="resizeMe()">
 		<p class="title">Broadband usage</p>
 		""" + """<p class="text-summary">{summary}</p>
 		<table id="summary">
@@ -197,15 +327,29 @@ if __name__ == "__main__":
 		<tr><th class="summary-title">Remaining</th><td>{this_rem:.3}</td></tr>
 		</tbody>
 		</table>
-		<p>{usage_chart}</p>
-		<p>{time_chart}</p>
-		<p>{backfill_chart}</p>
+		
+		<ul>
+		<li>{time_chart}</li>
+		<ul>
+		<li class="horiz">{usage_chart}</li>
+		<li class="horiz">{pop_chart}</li>		
+		</ul>
+		<li>{backfill_chart}</li>
+		<li>{usage_mon_chart}</li>
+		<li>{usage_day_chart}</li>
+		</ul>
+		<p>Generated: {now}</p>
 		</body>
-		</html""".format(summary=our_text, this_mon=this_mon, fcast=forecast,
+		</html>""".format(summary=our_text, this_mon=this_mon, fcast=forecast,
 				   this_rem=(this_mon.allowance - this_mon.bf)-this_mon.used,
 						backfill_chart=bf_chrt.render(disable_xml_declaration=True),
 						time_chart=time_cht.render(disable_xml_declaration=True), 
-						usage_chart=usage_pie.render(disable_xml_declaration=True))
+						usage_chart=usage_pie.render(disable_xml_declaration=True),
+						usage_mon_chart=usage_mon_chrt.render(disable_xml_declaration=True),
+						usage_day_chart=usage_day_chrt.render(disable_xml_declaration=True),
+						pop_chart=pop.render(disable_xml_declaration=True),
+						now=datetime.datetime.now().ctime()
+						)
 		
 		with open(args.output, "w") as html:
 			html.write(doc)
